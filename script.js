542a79590c5c1d2f798d0a82c6317121798ce5a3
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2023-10-29 07:44:38 -0300

    Possivel Solucao do Desafio

--- script.js
+++ script.js
@@ -7,14 +7,34 @@ function search() {
 
     $.getJSON(url, (user) => {
 
+        // document.getElementById("error").innerHTML = "";
+        clear();
+
         document.getElementById("name").innerHTML       = user.name;
         document.getElementById("html_url").innerHTML   = user.html_url;
         document.getElementById("company").innerHTML    = user.company;
 
         document.getElementById("avatar_url").innerHTML = `
-        <img src="${user.avatar_url}" alt="Imagem Usuario" width="220" height="220" class="shadow rounded"></img>
-        
+        <img src="${user.avatar_url}" alt="Imagem Usuario" width="220" height="220" class="shadow rounded"></img> 
         `;
 
+    }).fail( () => {
+        
+        clear();
+
+        document.getElementById("error").innerHTML = "<div class='alert alert-danger mt-1' role='alert'>Não Encontrado!</div>";
+
+        // document.getElementById("name").innerHTML       = "";
+        // document.getElementById("html_url").innerHTML   = "";
+        // document.getElementById("company").innerHTML    = "";
+        // document.getElementById("avatar_url").innerHTML = "";
     });
+}
+
+function clear() {
+        document.getElementById("error").innerHTML      = "";
+        document.getElementById("name").innerHTML       = "";
+        document.getElementById("html_url").innerHTML   = "";
+        document.getElementById("company").innerHTML    = "";
+        document.getElementById("avatar_url").innerHTML = "";
 }
