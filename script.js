5883be8eb0a1dac5b8ec1b33b002f00f48ed7d04
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2023-10-29 08:02:47 -0300

    Solucao do Desafio

--- script.js
+++ script.js
@@ -7,34 +7,33 @@ function search() {
 
     $.getJSON(url, (user) => {
 
-        // document.getElementById("error").innerHTML = "";
-        clear();
-
-        document.getElementById("name").innerHTML       = user.name;
-        document.getElementById("html_url").innerHTML   = user.html_url;
-        document.getElementById("company").innerHTML    = user.company;
-
-        document.getElementById("avatar_url").innerHTML = `
-        <img src="${user.avatar_url}" alt="Imagem Usuario" width="220" height="220" class="shadow rounded"></img> 
-        `;
+        setUserData(user);
+        clearError();
 
     }).fail( () => {
-        
-        clear();
 
-        document.getElementById("error").innerHTML = "<div class='alert alert-danger mt-1' role='alert'>Não Encontrado!</div>";
+        setUserData({});
+        showError("Não Encontrado!");
 
-        // document.getElementById("name").innerHTML       = "";
-        // document.getElementById("html_url").innerHTML   = "";
-        // document.getElementById("company").innerHTML    = "";
-        // document.getElementById("avatar_url").innerHTML = "";
     });
 }
 
-function clear() {
-        document.getElementById("error").innerHTML      = "";
-        document.getElementById("name").innerHTML       = "";
-        document.getElementById("html_url").innerHTML   = "";
-        document.getElementById("company").innerHTML    = "";
-        document.getElementById("avatar_url").innerHTML = "";
+function showError(msg) {
+    document.getElementById("error").innerHTML = `<div class='alert alert-danger mt-1' role='alert'>${msg}</div>`;
+}
+
+function clearError() {
+    document.getElementById("error").innerHTML = "";
+}
+
+function setUserData(user) {
+    document.getElementById("name").innerHTML       = user.name     || "";
+    document.getElementById("html_url").innerHTML   = user.html_url || "";
+    document.getElementById("company").innerHTML    = user.company  || "";
+
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ? `
+        <img src="${user.avatar_url}" alt="Imagem Usuario" width="220" height="220" class="shadow rounded"></img> 
+        `:
+        "";
+
 }
